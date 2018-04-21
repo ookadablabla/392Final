82802582cedec05537e595f4499f22442bca38ab
Quinten Hutchison <ookadablabla@quintens-macbook-pro.local>
2018-04-21 14:48:58 -0400

    Setup xcode project

---
+++ puzzlinator/puzzlinator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  puzzlinator
+//
+//  Created by Quinten Hutchison on 4/21/18.
+//  Copyright © 2018 EECS 392. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
