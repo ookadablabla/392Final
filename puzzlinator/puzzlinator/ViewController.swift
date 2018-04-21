ca62db2ba6466ee7e8fe46fc1e8393e8814128d3
Quinten Hutchison <ookadablabla@quintens-macbook-pro.local>
2018-04-21 15:28:29 -0400

    Added home page view

--- puzzlinator/puzzlinator/ViewController.swift
+++ puzzlinator/puzzlinator/ViewController.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let gameNames = ["Suduko", "Towers", "8-puzzle", "Tents"]
+    let imageNames = ["placeholder", "placeholder", "placeholder", "placeholder"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gameNames.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier:"cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
+        cell.myImage.image = UIImage(named: imageNames[indexPath.row] + ".png")
+        cell.myLabel.text = gameNames[indexPath.row]
+        
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
