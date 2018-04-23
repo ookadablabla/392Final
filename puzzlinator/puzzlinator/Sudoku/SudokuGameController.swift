cfd170970ba95d937b2cb30abb601a31fe8c67e5
Jacob Kessler <jacob.kessler@case.edu>
2018-04-23 10:54:58 -0400

    Initial commit for Sudoku game controller and view controller

---
+++ puzzlinator/puzzlinator/Sudoku/SudokuGameController.swift
@@ -0,0 +1,9 @@
+//
+//  SudokuGameController.swift
+//  puzzlinator
+//
+//  Created by Jacob Kessler on 4/23/18.
+//  Copyright © 2018 EECS 392. All rights reserved.
+//
+
+import Foundation
---
+++ puzzlinator/puzzlinator/Sudoku/SudokuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SudokuViewController.swift
+//  puzzlinator
+//
+//  Created by Jacob Kessler on 4/23/18.
+//  Copyright © 2018 EECS 392. All rights reserved.
+//
+
+import UIKit
+
+class SudokuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
